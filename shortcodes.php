9dc8421b81de09c39cda20fd6b0532d4e659699b
root <root@web-app-dev-ct-1.ufmg.br>
2024-12-18 17:56:39 +0000

    change some minors styles

--- shortcodes.php
+++ shortcodes.php
@@ -4,8 +4,6 @@ add_shortcode( 'site_fafar_custom_sidebar_menu', 'site_fafar_custom_sidebar_menu
 
 add_shortcode( 'site_fafar_highlight_area', 'site_fafar_highlight_area' );
 
-add_shortcode( 'site_fafar_carousel', 'site_fafar_carousel' );
-
 
 function site_fafar_custom_sidebar_menu() {
     
@@ -99,7 +97,7 @@ function get_menu_by_current_content() {
  */
 function site_fafar_highlight_area() {
 
-    if ( ! is_user_logged_in() ) return '';
+    if ( ! is_user_logged_in() || ! is_home() ) return '';
 
     if ( have_posts() ) {
 
@@ -119,9 +117,6 @@ function site_fafar_highlight_area() {
 
             $id_img      = get_field('imagem_servico');
             $url_img     = wp_get_attachment_url($id_img);
-
-            // print_r($servico);
-            // print_r('<br/>');
             
             $itens .= 
                     '<div class="col col-sm-6 col-md-4 highlight-card">
@@ -129,31 +124,13 @@ function site_fafar_highlight_area() {
                             <img src="' . $url_img . '" alt="Logo da ' . $title . '" />
                         </a>
                     </div>';
+
         }
 
     }
 
-    // return '
-    // <div class="container w-100">
-    //     <div class="row">
-    //         <h6>Destaques</h6>
-    //     </div>
-    //     <div class="row">
-    //         <div class="col col-sm-6 col-md-4 highlight-card">
-    //             <a href="https://www.farmacia.ufmg.br/escutas/" target="_blank" title="Ir para Escuta Acadêmica">
-    //                 <img src="https://www.farmacia.ufmg.br/wp-content/uploads/2024/12/escuta-fafar.png" alt="Logo da Escuta Acadêmica" />
-    //             </a>
-    //         </div>
-    //         <div class="col col-sm-6 col-md-4 highlight-card">
-    //             <a href="https://www.farmacia.ufmg.br/apresentacao-8/" target="_blank" title="Ir para Gerência Ambiental e Biossegurança">
-    //                 <img src="https://www.farmacia.ufmg.br/wp-content/uploads/2024/12/gerenciamento-ambiental-biosseguranca.png" alt="Logo da Gerência Ambiental e Biossegurança" />
-    //             </a>
-    //         </div>
-    //     </div>
-    // </div>';
-
     return '
-    <div class="container w-100">
+    <div class="container w-100 highlight-container">
         <div class="row">
             <h6>Serviços</h6>
         </div>
@@ -163,55 +140,3 @@ function site_fafar_highlight_area() {
     </div>';
 
 }
-
-function site_fafar_carousel() {
-
-    // return '
-    // <div id="carouselExample" class="carousel slide">
-    //     <div class="carousel-inner">
-    //         <div class="carousel-item active">
-    //         <img src="https://pxl-catawbaedu.terminalfour.net/fit-in/740x740/prod01/channel_2/media/catawba-college/site-assets/images/hedrick-lawn-1-720X720.webp" class="d-block" style="height: 360px" alt="...">
-    //         </div>
-    //         <div class="carousel-item">
-    //         <img src="https://pxl-catawbaedu.terminalfour.net/fit-in/740x740/prod01/channel_2/media/catawba-college/site-assets/images/hedrick-lawn-1-720X720.webp" class="d-block" style="height: 360px" alt="...">
-    //         </div>
-    //         <div class="carousel-item">
-    //         <img src="https://pxl-catawbaedu.terminalfour.net/fit-in/740x740/prod01/channel_2/media/catawba-college/site-assets/images/hedrick-lawn-1-720X720.webp" class="d-block" style="height: 360px" alt="...">
-    //         </div>
-    //     </div>
-    //     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    //         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    //         <span class="visually-hidden">Previous</span>
-    //     </button>
-    //     <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    //         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    //         <span class="visually-hidden">Next</span>
-    //     </button>
-    // </div>
-    // ';
-
-    // return '
-    //     <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-    //         <div class="carousel-inner">
-    //             <div class="carousel-item active">
-    //                 <img src="https://pxl-catawbaedu.terminalfour.net/fit-in/740x740/prod01/channel_2/media/catawba-college/site-assets/images/hedrick-lawn-1-720X720.webp" class="d-block" style="height: 360px" alt="...">
-    //             </div>
-    //             <div class="carousel-item">
-    //                 <img src="https://ufmg.br/thumbor/o1fdi3yWuzQA-y02iIcN-qOfhHM=/0x0:952x600/952x600/https://ufmg.br/storage/a/1/e/7/a1e7d90d08c8fbd84b61cb10b92b316d_15983899508573_856776441.jpg" class="d-block" style="height: 360px" alt="...">
-    //             </div>
-    //             <div class="carousel-item">
-    //                 <img src="https://dafarmaciaufmg.wordpress.com/wp-content/uploads/2011/11/cropped-faculdade-de-farmacia_imagelarge1.jpg" class="d-block" style="height: 360px" alt="...">
-    //             </div>
-    //         </div>
-    //     </div>';
-
-    // return '
-    //     <div class="carousel-fafar">
-    //         <img src="https://wallpapers.com/images/hd/macos-big-sur-1920-x-1080-background-uz3cwmapsoshabju.jpg" />
-    //     </div>
-    // ';
-
-    return '';
-
-}
-
